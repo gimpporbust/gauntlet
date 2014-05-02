e99033ca69b05c3a2934b84b6b5934923bb2a4e3
Luke Booth <Luke.Booth@cph.org>
2014-05-02 15:25:09 -0500

    epic fail

--- problems/problem001.rb
+++ problems/problem001.rb
@@ -1,6 +1,7 @@
 module Gauntlet
   module Problems
     class PostfixCalculator
+      attr_reader :expression, :index
 
       # This one's not math, I swear!
 
@@ -24,6 +25,39 @@ module Gauntlet
       # Source: Codewars
 
       def self.calc(expression)
+        @expression = expression.split(" ")
+        do_it(@expression, 0)
+      end
+
+      def self.do_it(expression, index)
+        set = expression[index..index+2]
+        if all_numbers(set)
+          # move on
+          index += 1
+        else
+          result = calculate(set)
+          expression[index] = result
+          index += 1
+        end
+        do_it(expression, index)
+      end
+
+      def self.all_numbers(set)
+        set.all? { |i| i.to_i != 0 }
+      end
+
+      def self.calculate(set)
+        case set[2]
+        when "+"
+          set[0].to_i + set[1].to_i
+        when "-"
+          set[0].to_i - set[1].to_i
+        when "*"
+          set[0].to_i * set[1].to_i
+        when "/"
+          set[0].to_i / set[1].to_i
+        else throw "NOPE"
+        end
       end
 
     end
